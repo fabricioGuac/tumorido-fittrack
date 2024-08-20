1bd6a14d799c5e8e1a82665374453b0551e4eb11
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-08-20 08:54:07 -0700

    migrates to aws-sdk v3

--- server/config/awsConfig.js
+++ server/config/awsConfig.js
@@ -1,14 +1,15 @@
-const AWS = require('aws-sdk');
+// Import the S3Client and necessary commands from the AWS SDK v3
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } =  require('@aws-sdk/s3-request-presigner');
 
-// Configures the AWS SDK with the necessary credentials and region to interact with the S3 bucket
-AWS.config.update({
-    accessKey:process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
-    region:'us-east-1'
-})
 
-// Creates an instance of the S3 service to interact with the S3 bucket
-const s3 = new AWS.S3(); 
+// Creates an instance of the S3 service with the necessary credentials and region 
+const s3 = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
 
-module.exports = s3;
-
+module.exports = {s3, PutObjectCommand, getSignedUrl, DeleteObjectCommand };
